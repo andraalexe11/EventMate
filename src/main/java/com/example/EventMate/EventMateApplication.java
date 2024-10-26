656f3919515fb079e15850c3ea43a07722c88970
Alexe Andra <ana.alexe@stud.ubbcluj.ro>
2024-10-26 17:08:59 +0300

    Initial commit

---
+++ src/main/java/com/example/EventMate/EventMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.EventMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EventMate/EventMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EventMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
